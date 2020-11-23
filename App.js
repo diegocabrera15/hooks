8c685d8255cb23069356248ebdce7e74b80eecd5
Diego Cabrera <diego.cabrera.15@outlook.com>
2020-11-22 21:55:24 -0500

    Use hook useCallback and example

--- App.js
+++ App.js
@@ -1,43 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useReducer, useMemo } from "react";
+import React, { useState, useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const initialState = {
-  count: 0,
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "increase": {
-      return { count: state.count + 1 };
-    }
-    case "decrease": {
-      return { count: state.count - 1 };
-    }
-    default: {
-      return state;
-    }
-  }
-};
-
-const users = [{name: 'Sandra', age: 35},{name: 'Chelsea', age: 26}]
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const totalAge = useMemo(() => {
-    let age = 0;
-    console.log('Calc...');
-    users.forEach(x => {
-      age = age + x.age
-    })
-    return age
-  }, [users])
-  console.log('Age total', totalAge);
+  const [count, setCount] = useState(0);
 
+  const increase = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+
+  const decrease = useCallback(() => {
+    setCount(count - 1);
+  }, [count]);
   return (
     <View style={styles.container}>
-      <Text onPress={() => dispatch({type: 'increase'})}>+</Text>
-  <Text style={styles.text}>Caddy {state.count}</Text>
-      <Text onPress={() => dispatch({type: 'decrease'})}>-</Text>
+      <Text onPress={() => increase()}>+</Text>
+      <Text style={styles.text}>Caddy {count}</Text>
+      <Text onPress={() => decrease()}>-</Text>
       <StatusBar style="auto" />
     </View>
   );
